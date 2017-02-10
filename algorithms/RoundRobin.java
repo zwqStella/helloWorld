d3b726d17ae9d7976c32192c5b3856ed8875f7d8
zwqStella <zwqStella@hotmail.com>
2017-02-10 13:49:28 +0800

    Update RoundRobin.java

--- algorithms/RoundRobin.java
+++ algorithms/RoundRobin.java
@@ -3,50 +3,31 @@
 */
 
 public class RoundRobin{
-  public void race(int k){
-        int[][] table = new int[2*k][2*k];
-        for(int i = 0;i < 2*k ;i++){
-            table[i][0]= i;
+  public void arrange(int n)  {  		
+		int teams = (int) Math.pow(2, n); 		//the number of teams
+		int [][] table = new int[teams][n+1]; 		//the table to store results
+		
+        for(int i = 0; i < teams; i++) {
+        	  table[i][0]=i;  		//initialize the first line with the teams' name
         }
-        arrange(table,0,2*k-1,0,2*k-1);
-        for(int i = 0;i < 2*k;i++){
-            for(int j = 0;j < 2*k;j++){
-                System.out.print("   "+table[i][j]);
-            }
-            System.out.print("\n");
-        }
-  }
-  
-  public void arrange(int[][] table,int days,int date,int start,int end){
-        int mid = (start+end)/2;
-        if(days < date){
-            arrange(table,days,(days+date)/2,start,mid);
-            arrange(table,days,(days+date)/2,mid+1,end);
-            swap(table,days,date,start,mid,end);
-        }
-  }
-  
-  public void swap(int[][] table,int days,int date,int start,int mid,int end){
-        if(date - days == 2){
-            table[start][days + 1] = table[mid][days];
-            table[mid][days + 1] = table[end][days];
-            table[end][days + 1] = table[start][days];
-            table[start][days + 2] = table[end][days];
-            table[mid][days + 2] = table[start][days];
-            table[end][days + 2] = table[mid][days];
-        }else{
-            int i = start,j = mid+1;
-            int m = days,n = (days+date)/2 + 1;
-            while(n <= date){
-                while(j <= end){
-                    table[i][n] = table[j][m];
-                    table[j][n] = table[i][m];
-                    i++;j++;
-            }
-            i = start;j = mid+1;
-            m++;n++;
-        }
-        }
-  }
-  
+        
+        int m = 1; 
+        for(int s=1; s<=n; s++)  
+        {  
+        	teams /= 2;  
+            for(int t=1; t<=teams; t++)  
+            {  
+                for(int i=m+1; i<=2*m; i++) 
+                {  
+                    for(int j=m+1; j<=2*m; j++) 
+                    {  
+                        table[i][j+(t-1)*m*2] = table[i-m][j+(t-1)*m*2-m]; 
+                        table[i][j+(t-1)*m*2-m] = table[i-m][j+(t-1)*m*2]; 
+                    }  
+                      
+                }  
+            }  
+            m *= 2;  
+        }  
+    }  
 }
