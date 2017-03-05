49df3ebf5d6a96df67bd66a2dc0f07aab6b52752
钟文清 <zwqStella@hotmail.com>
2017-03-05 23:09:15 +0800

    Update stream.java

--- algorithms/stream.java
+++ algorithms/stream.java
@@ -1,7 +1,7 @@
 /*
   learning to use stream of java 8
 */
-//清单 1. Java 7 的排序、取值实现
+// Java 7 的排序、取值实现
 List<Transaction> groceryTransactions = new Arraylist<>();
 for(Transaction t: transactions){
  if(t.getType() == Transaction.GROCERY){
@@ -18,7 +18,7 @@ for(Transaction t: groceryTransactions){
  transactionsIds.add(t.getId());
 }
 //而在 Java 8 使用 Stream，代码更加简洁易读；而且使用并发模式，程序执行速度更快。
-//清单 2. Java 8 的排序、取值实现
+//Java 8 的排序、取值实现
 List<Integer> transactionsIds = transactions.parallelStream().
  filter(t -> t.getType() == Transaction.GROCERY).
  sorted(comparing(Transaction::getValue).reversed()).
